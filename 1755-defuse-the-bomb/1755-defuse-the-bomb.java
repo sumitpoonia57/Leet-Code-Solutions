3b8eca0c503f9857a1fbf213f7a635e33f85bd11
Sumit <90952998+sumitpoonia57@users.noreply.github.com>
2023-10-21 22:13:24 +0530

    Time: 2 ms (68.54%), Space: 40.3 MB (100%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -1,9 +1,7 @@
 class Solution {
     public int[] decrypt(int[] code, int k) {
         int ans[]=new int[code.length];
-        if(k==0){
-            Arrays.fill(ans,0);
-        }
+        
         if(k>0){
             for(int i=0;i<code.length;i++){
                 int sum=0;
@@ -14,10 +12,10 @@ class Solution {
             }
         }
         if(k<0){
-            int x=(-1)*k;
+          
            for(int i=0;i<code.length;i++){
                int sum=0;
-               for(int j=1;j<=x;j++){
+               for(int j=1;j<=(-1)*k;j++){
                      sum+=code[((i-j)+code.length)%code.length];
                }
                ans[i]=sum;
