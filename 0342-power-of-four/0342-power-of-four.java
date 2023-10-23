9073ed836385e7d4c2713ce8c295020eabf483c7
Sumit <90952998+sumitpoonia57@users.noreply.github.com>
2023-10-23 13:25:18 +0530

    Time: 1 ms (76.31%), Space: 39.4 MB (58.82%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.java
+++ 0342-power-of-four/0342-power-of-four.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n==2147483647){
-            return false;
-        }
-        int x=(int)Math.sqrt(n);
         if(n<=0){
             return false;
         }
-        if(n==1){
-            return true;
-        }
-        for(int i=0;i<x;i++){
+        for(int i=0;i<=15;i++){
             int ans=(int)Math.pow(4,i);
             if(ans==n){
                 return true;
